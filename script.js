4c26bc9b7230beed43e9c61c1905182f06b46702
Pinx-pinx <pinkybenitez3364@gmail.com>
2026-01-11 23:59:44 +0100

    add function render episodes

--- script.js
+++ script.js
@@ -2,20 +2,43 @@ const allEpisodes = getAllEpisodes();
 const root = document.getElementById("root");
 
 function formatEpisodeCode(season, number) {
-  return `S${String(season).padStart(2, "0")}E${String(number).padStart(2, "0")}`;
+  return `S${String(season).padStart(2, "0")}E${String(number).padStart(
+    2,
+    "0"
+  )}`;
 }
+function renderEpisodes(episodes) {
+  root.innerHTML = "";
 
-allEpisodes.forEach((episode) => {
-  const episodeDiv = document.createElement("div");
-  episodeDiv.className = "episode";
+  episodes.forEach((episode) => {
+    const episodeDiv = document.createElement("div");
+    episodeDiv.className = "episode";
+    episodeDiv.innerHTML = `
+      <h2>${episode.name} (${formatEpisodeCode(
+      episode.season,
+      episode.number
+    )})</h2>
+      <img src="${episode.image.medium}" alt="${episode.name}">
+      <p>Season: ${episode.season}</p>
+      <p>Episode: ${episode.number}</p>
+      <div>${episode.summary}</div>
+    `;
+    root.appendChild(episodeDiv);
+  });
+}
+renderEpisodes(allEpisodes);
 
-  episodeDiv.innerHTML = `
-    <h2>${episode.name} (${formatEpisodeCode(episode.season, episode.number)})</h2>
-    <img src="${episode.image.medium}" alt="${episode.name}">
-    <p>Season: ${episode.season}</p>
-    <p>Episode: ${episode.number}</p>
-    <div>${episode.summary}</div>
-  `;
+const searchInput = document.getElementById("search-input");
+const matchCount = document.getElementById("match-count");
 
-  root.appendChild(episodeDiv);
-});
+function searchEpisodes(searchTerm) {
+  const filteredEpisodes = [];
+
+  allEpisodes.forEach((episode) => {
+    if (
+      episode.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      episode.summary.toLowerCase().includes(searchTerm.toLowerCase())
+    ) {
+      filteredEpisodes.push(episode);
+    }
+  });
